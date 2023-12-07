59196dde62d8310e7244e165720249c4d4464443
faisalh2r <faisal2100018402@wenmail.uad.ac.id>
2023-12-07 11:21:48 +0700

    fix 2

--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -5,10 +5,10 @@ import { refreshToken } from "../controllers/RefreshToken.js";
 
 const router = express.Router();
 
-router.get('/users', verifyToken, getUsers);
+router.get('/users', getUsers);  
 router.post('/users', Register);
 router.post('/login', Login);
-router.get('/token', refreshToken);
+router.get('/token', verifyToken, refreshToken);
 router.delete('/logout', Logout);
 
 export default router;
