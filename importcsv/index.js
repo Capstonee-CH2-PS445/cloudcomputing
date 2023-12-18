ea805605241cf386383ca4ab1f81798a44f67243
HajrilMalik <hajrilmalik82@gmail.com>
2023-12-18 18:24:39 +0700

    fix export

--- importcsv/index.js
+++ importcsv/index.js
@@ -1,23 +1,56 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const mysql = require('mysql');
+
+// Konfigurasi koneksi database
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root', // Ganti dengan username MySQL Anda
     password: '', // Ganti dengan password MySQL Anda
     database: 'auth_db' // Ganti dengan nama database Anda
 });
-const csv = require('csvjson')
-const knex = require('./knex')
-const fs = require('fs')
 
-const file = fs.readFileSync('data.csv', 'utf8')
-const dataObj = csv.toObject(file)
+const namaFileCSV = 'Book.csv'; // Ganti dengan nama file CSV Anda
 
-knex.insert(dataObj)
-    .into('employees')
-    .then(() => {
-        console.log('Import data done!')
-        process.exit(0)
+// Array untuk menyimpan data dari CSV
+const data = [];
+
+// Membaca file CSV
+fs.createReadStream(namaFileCSV)
+    .pipe(csv({ separator: ',' }))  // Sesuaikan karakter pemisah dengan file CSV Anda
+    .on('data', (row) => {
+        // Menghilangkan spasi ekstra dari setiap nilai dan mengganti nilai NULL dengan null
+        const cleanedRow = Object.fromEntries(
+            Object.entries(row).map(([key, value]) => [key, value.trim() === '' ? null : value.trim()])
+        );
+        data.push(cleanedRow);
     })
-    .catch(() => {
-        console.log('Import data failed')
-        process.exit(0)
+    .on('end', () => {
+        // Mengimpor data ke database
+        connection.connect((err) => {
+            if (err) {
+                console.error('Error koneksi ke database:', err.message);
+                return;
+            }
+
+            // Menyusun query untuk mengimpor data ke database
+            const query = 'REPLACE INTO books (id_user, id, book_id, isbn, authors, original_publication_year, title, average_rating, ratings_count, small_image_url) VALUES ?';
+            const values = data.map((row) => [row.id_user, row.id, row.book_id, row.isbn, row.authors, row.original_publication_year, row.title, row.average_rating, row.ratings_count, row.small_image_url]);
+
+            // Menjalankan query
+            connection.query(query, [values], (error, results) => {
+                if (error) {
+                    console.error('Error mengimpor data ke database:', error.message);
+                } else {
+                    console.log('Data yang diimpor:', values);
+                    console.log('Data berhasil diimpor ke database.');
+                }
+
+                // Menutup koneksi database
+                connection.end();
+            });
+        });
     })
+    .on('error', (error) => {
+        console.error('Error membaca file CSV:', error.message);
+    });
