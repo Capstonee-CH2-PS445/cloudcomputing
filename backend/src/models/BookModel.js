bec6126cb9b1b9b86a7c72913bfe6233e530d7d5
HajrilMalik <hajrilmalik82@gmail.com>
2023-12-18 18:06:47 +0700

    book fix

--- backend/src/models/BookModel.js
+++ backend/src/models/BookModel.js
@@ -1,30 +1,64 @@
 // BookModel.js
 import { Sequelize } from "sequelize";
 import db from "../config/Database.js";
-
+import Users from "../models/UserModel.js"
 const { DataTypes } = Sequelize;
 
+
 const Book = db.define('books', {
-    id: {
-        autoIncrement: true,
+    id_user: {
         type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    id: {
+        type: DataTypes.INTEGER,
+    },
+    book_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
         primaryKey: true
     },
+    isbn: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+    },
+    authors: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    original_publication_year: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
     title: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    author: {
-        type: DataTypes.STRING
+    average_rating: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
     },
-    description: {
-        type: DataTypes.TEXT
+    ratings_count: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    small_image_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     isBookmarked: {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
-    }
+    },
+    isCurrentlyReading: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    },
 }, {
     freezeTableName: true
 });
+
+// Definisikan relasi dengan User
+Book.belongsTo(Users, { foreignKey: 'id_user' });
 
 export default Book;
--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -2,7 +2,7 @@ import express from "express";
 import { getUsers, Register, Login, Logout } from "../controllers/Users.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
-import { addBook, updateBook, deleteBook, getBooks, addBookmark, removeBookmark, getBookById } from "../controllers/BookController.js";
+import { getBooks, getBookById } from "../controllers/BookController.js";
 
 const router = express.Router();
 
@@ -14,11 +14,6 @@ router.post('/refreshToken', refreshToken);
 router.delete('/logout', Logout);
 // buku
 router.get('/books', getBooks);
-router.post('/books', verifyToken, addBook);
-router.put('/books/:bookId', updateBook);
-router.delete('/books/:bookId', verifyToken, deleteBook);
-router.post('/books/:bookId/bookmark', addBookmark);
-router.delete('/books/:bookId/bookmark', removeBookmark);
 router.get('/books/:bookId', getBookById);
 
 export default router;
